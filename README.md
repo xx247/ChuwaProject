9cd9b1e3ceacaa05b8b286cc88b74c78ac7d1cca
HintNeeder <124347487+HintNeeder@users.noreply.github.com>
2025-01-07 16:55:04 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# chuwa-project1
+Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import ProductList from "./pages/ProductList";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main style={{ flex: 1, padding: "1rem" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/products" element={<ProductList />} />
+          {/* Add more routes as you build out pages */}
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
