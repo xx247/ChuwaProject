46e54462747efa0a24acc4dec012c58f55ba5572
HintNeeder <124347487+HintNeeder@users.noreply.github.com>
2025-01-11 23:10:31 -0800

    Add files via upload

---
+++ A_ProductManagementSystem/vite-c/src/App.jsx
@@ -0,0 +1,31 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/cart";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Header />
+      <main id="root">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          {/* <Route path="/cart" element={<Cart />} /> */}
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ A_ProductManagementSystem/vite-c/src/services/productData.jsx
@@ -0,0 +1,44 @@
+const productData = [
+  {
+    id: 1,
+    name: "coke",
+    price: 3,
+    url: "src/services/image/coke.jpg",
+    type: "image/jpg",
+    qnty:0,
+  },
+  {
+    id: 2,
+    name: "coffee",
+    price: 5,
+    url: "src/services/image/coffee.jpg",
+    type: "image/jpg",
+    qnty:0,
+  },
+  {
+    id: 3,
+    name: "fanta",
+    price: 2,
+    url: "src/services/image/fanta.jpg",
+    type: "image/jpg",
+    qnty:0,
+  },
+  {
+    id: 4,
+    name: "pepper",
+    price: 4,
+    url: "src/services/image/pepper.jpg",
+    type: "image/jpg",
+    qnty:0,
+  },
+  {
+    id: 5,
+    name: "power",
+    price: 1,
+    url: "src/services/image/power.jpg",
+    type: "image/jpg",
+    qnty:0,
+  },
+];
+
+export default productData;
