b76256c7c5240180bede648d7c1d1b058df71928
Xiaoxue <xx97247@gmail.com>
2025-01-18 16:35:15 -0500

    Create User.js

---
+++ B_EmployeeManagementProject/server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, default:'username' },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['employee', 'HR'], default: 'user' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
