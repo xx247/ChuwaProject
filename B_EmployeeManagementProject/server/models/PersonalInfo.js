8258998559d726240baf25eaba0bd33dd0ed0a99
Xiaoxue <xx97247@gmail.com>
2025-01-19 14:58:49 -0500

    personal info

--- B_EmployeeManagementProject/server/models/PersonalInfo.js
+++ B_EmployeeManagementProject/server/models/PersonalInfo.js
@@ -12,19 +12,42 @@ const PersonalInfoSchema = new mongoose.Schema({
     city: String,
     state: String,
     zip: String,
+    required: true
   },
-  contactInfo: {
-    cellPhone: String,
-    workPhone: String,
+  cellPhone: {String, required:true},
+  workPhone: String,
+  ssn: {String, required:true},
+  dob: {Date, required:true},
+  gender: { type: String, enum: ['Male', 'Female', 'I do not wish to answer'], required:true},
+  citizenshipStatus: {
+    type: String,
+    // work authorization
+    enum: ['Green Card', 'Citizen', 'H1-B', 'L2', 'F1(CPT/OPT)', 'H4', 'Other'],
+    required:true
   },
-  ssn: String,
-  dob: Date,
-  gender: { type: String, enum: ['Male', 'Female', 'I do not wish to answer'] },
-  employmentDetails: {
-    visaStatus: String,
-    visaStartDate: Date,
-    visaEndDate: Date,
+  workAuthorization: {
+    visaTitle: { type: String },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    files: [{ type: String }], // File paths or URLs
   },
+  reference: {
+    firstName: { type: String, required:true },
+    lastName: { type: String, required:true },
+    middleName: { type: String },
+    phone: { type: String },
+    email: { type: String },
+    relationship: { type: String, required:true },
+  },
+  emergencyContacts: 
+    {
+      firstName: { type: String, required:true },
+      lastName: { type: String, required:true },
+      middleName: { type: String },
+      phone: { type: String },
+      email: { type: String },
+      relationship: { type: String, required:true },
+    }
 });
 
 module.exports = mongoose.model('PersonalInfo', PersonalInfoSchema);
